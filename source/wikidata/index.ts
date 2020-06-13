d0dd0900be849f00644e387ac57fffe790af3a77
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-13 13:54:41 +0200

    feat: after workshop

--- source/wikidata/index.ts
+++ source/wikidata/index.ts
@@ -1,5 +1,6 @@
 import got from 'got'
 import randomItem from 'random-item'
+import {/*sparqlQuery,*/ getEntities} from 'wikidata-sdk-got'
 
 const wdk = require('wikidata-sdk')
 
@@ -10,5 +11,21 @@ async function downloadJson(url: string): Promise<any> {
 }
 
 export async function getItem(): Promise<string> {
-	return 'TODO'
+	const query = `SELECT ?item ?itemLabel ?itemDescription WHERE {
+?item wdt:P31 wd:Q337060.
+FILTER EXISTS {?item wdt:P18 ?image.}
+SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}`
+	const url = wdk.sparqlQuery(query)
+	const body = await downloadJson(url)
+
+	// const body = await sparqlQuery(query, GOT_OPTIONS)
+	const one = randomItem(body.results.bindings) as any
+	const itemUrl = one.item.value
+
+	const entityInfos = await getEntities({ids: 'Q1'})
+	const universe = entityInfos.Q1.labels!.de.value
+
+	console.log('universe label de', universe)
+	return itemUrl
 }
